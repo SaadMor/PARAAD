c27a30982f6d05c59c7650e4cf8f4af7f868165c
SaadMor <167303882+SaadMor@users.noreply.github.com>
2026-04-14 10:38:08 +0100

    Add icon-based contact and metadata styling

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -1,3 +1,4 @@
+import { FiCalendar, FiMapPin } from 'react-icons/fi';
 import type { ExperienceItem } from '../data/resume';
 import SectionTitle from './SectionTitle';
 
@@ -14,12 +15,18 @@ const ExperienceSection = ({ heading, items }: ExperienceSectionProps) => {
         {items.map((item) => (
           <li key={`${item.role}-${item.company}`}>
             <h3>{item.role}</h3>
-            <p>
-              <a className="link-like" href={item.companyUrl} rel="noreferrer" target="_blank">
-                {item.company}
-              </a>
+            <p className="meta meta-inline">
+              <span className="icon-inline">
+                <FiMapPin className="inline-icon" aria-hidden="true" focusable="false" />
+                <a className="link-like" href={item.companyUrl} rel="noreferrer" target="_blank">
+                  {item.company}
+                </a>
+              </span>
+              <span className="icon-inline">
+                <FiCalendar className="inline-icon" aria-hidden="true" focusable="false" />
+                <span>{item.period}</span>
+              </span>
             </p>
-            <p className="meta">{item.period}</p>
             <ul>
               {item.bullets.map((bullet) => (
                 <li key={bullet}>{bullet}</li>
